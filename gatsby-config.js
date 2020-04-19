de72eb4408446616a5830ac29a2ed46bc23f8c56
Josh White <joshwhite@Joshs-iMac.local>
2020-04-19 10:39:50 -0400

    Add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         background_color: `#fde8e8`,
         theme_color: `#fde8e8`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, This path is relative to the root of the site.
+        icon: `src/images/crown.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
